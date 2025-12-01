47dcee24f6065b2f6ef2ae159812b61d9626e079
luka <luka.krnjevo@gmail.com>
2025-12-01 19:17:13 +0000

    solve day 1

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,11 +1,59 @@
 advent_of_code::solution!(1);
 
+fn parse_lines(input: &str) -> Vec<&str> {
+    input.lines().filter(|l| !l.is_empty()).collect()
+}
+
 pub fn part_one(input: &str) -> Option<u64> {
-    None
+    // Read string file input into a vector
+    let lines = parse_lines(input);
+
+    let mut count = 0; // number of zeros
+    let mut current = 50; // starting
+
+    for line in lines.iter() {
+        // println!("{}", line)
+        let dir = line.chars().next().unwrap();
+        let amount: i32 = line[1..].parse().unwrap();
+
+        println!("dir = {}, amount = {}", dir, amount);
+
+        if dir == 'L' {
+            current = (current - amount).rem_euclid(100);
+        } else {
+            current = (current + amount).rem_euclid(100);
+        }
+
+        if current == 0 {
+            count += 1;
+        }
+    }
+    println!("count = {}", count);
+    Some(count as u64)
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+    let lines = parse_lines(input);
+
+    let mut count: u64 = 0; // number of zeros
+    let mut current: i32 = 50; // starting
+
+    for line in lines {
+        let dir = line.chars().next().unwrap();
+        let amount: i32 = line[1..].parse().unwrap();
+
+        let step = if dir == 'L' { -1 } else { 1 };
+
+        // go 1 by 1
+        for _ in 0..amount {
+            current = (current + step).rem_euclid(100);
+            if current == 0 {
+                count += 1;
+            }
+        }
+    }
+
+    Some(count)
 }
 
 #[cfg(test)]
